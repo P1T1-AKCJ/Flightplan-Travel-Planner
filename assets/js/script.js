c6b67e6c2821572ad7abb6a5d74d59ab07c006f7
Kiki <kirstinlisnoff5@gmail.com>
2025-01-16 21:47:00 -0500

    Co-authored-by: antoninast <antoninast@users.noreply.github.com> Co-authored-by: Christopher Lin <christophermlin@users.noreply.github.com> Co-authored-by: Jaden Szewczak <jadenszewczak@users.noreply.github.com> added dropdown menu, and submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-const notYetStarted = [{ text: '', status: 'not-yet' }];
+const todos = [];
 
+const statusDropdown = document.createElement('div');
+statusDropdown.setAttribute("class", "dropdown");
+statusDropdown.innerHTML = 
+`
+    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+        Status 
+    </button>
+    <ul class="dropdown-menu">
+      <li><a class="dropdown-item" href="#">Not Started</a></li>
+      <li><a class="dropdown-item" href="#">In Progress</a></li>
+      <li><a class="dropdown-item" href="#">Completed</a></li>
+    </ul>
+`;    
 
 // form submission
 document.getElementById("userForm").addEventListener("submit", function(event) {
@@ -7,30 +20,28 @@ document.getElementById("userForm").addEventListener("submit", function(event) {
 
     // get user input
     let userInput = document.getElementById("inputField").value;
-    
-    notYetStarted.push({ text: userInput, status: 'not-yet'});
+    console.log('userInput', userInput);
 
+    todos.push({ index: todos.length + 1, text: userInput, status: 'not-yet'});
     // create new list item
-    const li = document.createElement("li");
-    li.textContent = userInput;
-    li.classList.add('todo-item');
-
-    // next button idea
-    const nextButton = document.createElement("button");
-    nextButton.textContent = "Next";
-    nextButton.addEventListener("click", function() {
-        
-        
-    })
-
-    li.appendChild(nextButton);
+    const divTodo = document.createElement("div");
+    divTodo.textContent = userInput;
+    divTodo.classList.add('todo-item');
+    divTodo.appendChild(statusDropdown.cloneNode(true));
     
     // Append the new list item to the existing list
-    document.getElementById("myList").appendChild(li);
+    document.getElementById("myList").appendChild(divTodo);
 
     // clear input field after submit
     document.getElementById("inputField").value = "";
     
     // log for debugging
-    console.log("User input:", userInput, notYetStarted);
+    console.log("User input:", userInput, todos);
+
+    //Store information in local storage?
 });
+
+
+// dropdown
+// delete button
+// localStorage
